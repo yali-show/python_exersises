dca5ea8991b6883658037c6f05d5e28728a0ed39
Ilya Bilyi <ya.ilya.white@gmail.com>
2021-06-11 23:00:43 +0300

    each second char of words to upper

--- solutions_day2/strange_string.py
+++ solutions_day2/strange_string.py
@@ -1,13 +1,29 @@
 #made function with loop (for) and if/else
-def strange_function(string):
+def modefy_string(string):
     result = ""
-    sl = True  # capitalize
+    is_char = True  # capitalize
     for letter in string:
-        if sl:
+        if is_char:
             result += letter.lower()
         else:
             result += letter.upper()
         if letter != ' ':
-            sl = not sl
+            is_char = not is_char
     return result
-print(strange_function(str(input('Could you pls, write any words:'))))
+#print(modefy_string(str(input('Could you pls, write any words:'))))
+
+
+def stranger_string(string):
+    splited_string = string.split()
+    str = ""
+    print(splited_string)
+    for element in splited_string:
+        for i in range(len(element)):
+
+            if i % 2 == 1:          # is it odd
+                str = str + element[i].upper()
+            else:
+                str = str + element[i]
+    return str
+if __name__ == '__main__':
+    print(stranger_string(str(input('Type your sentence: '))))
