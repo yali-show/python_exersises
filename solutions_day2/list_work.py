d6b9ce5481703b55c2715b24bc7069b697417992
Ilya Bilyi <ya.ilya.white@gmail.com>
2021-06-11 22:59:49 +0300

    threw out repeated numbers from list

--- solutions_day2/list_work.py
+++ solutions_day2/list_work.py
@@ -1,2 +1,6 @@
+def identic(marks):
+    return set(marks)
+
 mark_list = [10, 11, 2, 3, 5, 8, 23, 11, 2, 5, 76, 43, 2, 32, 76, 3, 10, 0, 1]
-print(set(mark_list))
+result = identic(mark_list)
+print(result)
