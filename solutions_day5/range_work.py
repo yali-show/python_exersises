1e6b11948edc680a1ecea6f72f2a9f6da59ee49f
Ilya Bilyi <ya.ilya.white@gmail.com>
2021-07-12 20:12:42 +0300

    refactoring

--- solutions_day5/range_work.py
+++ solutions_day5/range_work.py
@@ -1,6 +1,7 @@
 '''
+****************************************************
 function calculate range of even numbers
-*********************************
+****************************************************
 x , y - int values
 x must be less than y
 return: list of even numbers placed between x and y
@@ -11,9 +12,9 @@ def even_numbers_range(x, y):
     first_number_in_range = x
     last_number_in_range = y
     step = 2
-    #to check if first number is odd we can skip it
-    #in our next constructions our range started i made first number as even
-    #it gave me possibilitiy to make step equal 2
+    # to check if first number is odd we can skip it
+    # in our next constructions our range started i made first number as even
+    # it gave me possibility to make step equal 2
 
     if first_number_in_range % 2 != 0:
         first_number_in_range += 1
--- solutions_day5/students_marks_dict.py
+++ solutions_day5/students_marks_dict.py
@@ -1,27 +1,36 @@
 sampleDict = {
-   "class":{
-      "student":{
-         "name":"Mike",
-         "marks":{
-            "physics":70,
-            "history":80
+   "class": {
+      "student": {
+         "name": "Mike",
+         "marks": {
+            "physics": 70,
+            "history": 80
          }
       }
    }
 }
 
-
+'''
+*******************************************************
+function calculate average student's mark in dictionary
+*******************************************************
+return float
+'''
 def student_average_score(sample_dict) -> float:
     class_of_student = sample_dict["class"]
     students_data = class_of_student["student"]
     marks = students_data["marks"]
     sum_of_numbers = 0
     count = 0
+    # summed all numbers in values by keys and counted them
+
     for key in marks:
         sum_of_numbers += marks[key]
         count += 1
+
     quantity_of_numbers = count
     average_marks = float(sum_of_numbers // quantity_of_numbers)
     return average_marks
 
-print(student_average_score(sampleDict))
+if __name__ == '__main__':
+    print(student_average_score(sampleDict))
