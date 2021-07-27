05fe96cf0c775c4f50aed26589ea3f655292ea7d
Ilya Bilyi <ya.ilya.white@gmail.com>
2021-07-27 15:46:18 +0300

    refactoring

--- solutions_day6/class_rectangle.py
+++ solutions_day6/class_rectangle.py
@@ -1,19 +1,19 @@
 class Rectangle:
-    def __init__(self, lenght=0, weight=0):
-        self.lenght = lenght
-        self.weight = weight
+    def __init__(self, length=0, width=0):
+        self.length = length
+        self.width = width
 
     def calculate_area(self):
-        area = self.lenght * self.weight
+        area = self.length * self.width
 
         return area
 
 if __name__ == '__main__':
     message = input('Do you want add arguments?\nYes or No?: ')
     if message.lower() == 'yes':
-        lenght = int(input('Lenght: '))
-        weidth = int(input('Weidth: '))
-        result = Rectangle(lenght, weidth).calculate_area()
+        length = int(input('Length: '))
+        width = int(input('Width: '))
+        result = Rectangle(length, width).calculate_area()
         print(f'Your result: {result}')
     else:
         print(Rectangle().calculate_area())
